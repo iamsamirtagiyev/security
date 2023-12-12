d3faabe1fdfcba4e7c0858a7aa5b37adbfffefd1
amaliyamiriyeva8 <stagiyev579@gmail.com>
2023-12-12 17:00:12 +0400

    s

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,17 @@
+const security = "http://localhost:3000/security"
+const favorite = "http://localhost:3000/favorite"
+
 const nav = document.querySelector('nav')
 const icon = document.querySelector('.icon')
 const menu = document.querySelector('.menu')
+const cardWrapper = document.querySelector('.card-wrapper')
 
-window.onscroll = ()=>{
-    if(window.scrollY > 30){
+
+window.onscroll = () => {
+    if (window.scrollY > 30) {
         nav.style.background = 'rgba(0, 0, 0, .8)'
     }
-    else{
+    else {
         nav.style.background = 'transparent'
     }
 }
@@ -21,3 +26,29 @@ icon.addEventListener('click', () => {
         icon.classList.replace('bx-x', 'bx-menu')
     }
 })
+
+fetch(security).then(response => response.json()).then(data => {
+    data.forEach(item => {
+        cardWrapper.innerHTML += `
+        <div class="card" onclick="goToDetails(${item.id})">
+            <i class="bx bx-heart favorite")></i>
+            <div class="image">
+                <img src="${item.url}" alt="card">
+            </div>
+            <div class="about">
+                <h2 class="title">${item.name}</h2>
+                <p>${item.description}</p>
+            </div>
+            <div class="buttons">
+                <button>Update</button>
+                <button onclick="deleteItem(${item.id})">Delete</button>
+            </div>
+        </div>`
+    });
+})
+
+const deleteItem = (id) =>{
+    axios.delete(`${security}/${id}`).then(res => window.location.reload())
+}
+
+const goToDetails = (id) => window.location = `details.html?id=${id}`
